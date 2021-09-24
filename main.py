8b7f710ad303fc74aa8996b84c991cd140507346
Ashok Bakthavathsalam <kgashok@users.noreply.github.com>
2021-09-24 03:52:01 +0000

    using auto-format

--- main.py
+++ main.py
@@ -1,32 +1,19 @@
 import ast
 # Read the values from standard in
-instring = input().strip() 
+instring = input().strip()
 print(instring)
 alist = None
 try:
-	# what if input is in the form of [1, 2, 4, -2]?
-	alist = ast.literal_eval(instring)
-	assert type(alist) == list 
+    # what if input is in the form of [1, 2, 4, -2]?
+    alist = ast.literal_eval(instring)
+    assert type(alist) == list
 except:
-	# what if input is just simple 1 390 4 -4 4?
-	alist = instring.split(' ')
-	alist = list(map(int, alist))
-	print(alist)
+    # what if input is just simple 1 390 4 -4 4?
+    alist = instring.split(' ')
+    alist = list(map(int, alist))
+    print(alist)
 #-------------------------------------------
 
 # Write your Typical indexed version below
 
-
-
-
-
-
-
-
-
 # Write a more Pythonic version below
-
-
-
-
-
